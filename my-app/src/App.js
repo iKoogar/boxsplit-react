9841680de3eb3107f85fb95c10ea19ac9c8d871a
kurokonobaskue <49701177+kurokonobaskue@users.noreply.github.com>
2020-06-02 11:58:08 -0400

    seeing if the website works without....
    having the elements that the react code had when we first made the website.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,53 +12,56 @@ function exportWithObjectId() {
 
 function App() {
   return (
-    <div className="App">
-      <ul className="navbar">
-        <li className="buttons">
-          <a href="#">home</a>
-        </li>
+    <div>
+      <div className="App">
+        <ul className="navbar">
+          <li className="buttons">
+           <a href="#">home</a>
+          </li>
 
-        <li className="buttons">
-          <a href="#">search</a>
-        </li>
+          <li className="buttons">
+            <a href="#">search</a>
+          </li>
 
-        <li className="buttons">
-          <a href="#">boxsplit</a>
-        </li>
+          <li className="buttons">
+            <a href="#">boxsplit</a>
+          </li>
 
-        <li className="right">
-          <a href="#"><FiLogOut/></a>
-        </li>
+          <li className="right">
+            <a href="#"><FiLogOut/></a>
+          </li>
 
-        <li className="right">
-          <a href="#"><BsFillGearFill/></a>
-        </li>
+          <li className="right">
+            <a href="#"><BsFillGearFill/></a>
+          </li>
 
-        <li className="right">
-          <a href="#profile"><FaUserAlt/></a>
-        </li>
+          <li className="right">
+            <a href="#profile"><FaUserAlt/></a>
+          </li>
 
-      </ul>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consectetur facilisis nunc at 
-      imperdiet. Quisque interdum, massa vel semper euismod, libero quam lobortis lacus, a bibendum 
-      sapien diam eget nibh. Fusce elementum nec diam et consequat. Donec condimentum arcu at arcu 
-      commodo dignissim. Mauris porttitor euismod erat sed luctus. Nunc malesuada mi faucibus mauris 
-      accumsan, eu luctus velit mattis. Cras id hendrerit odio. Mauris egestas, nibh id blandit accumsan, 
-      mi enim dignissim turpis, sed lacinia sapien turpis nec velit. Donec fringilla nibh sit amet mauris
-      pellentesque sagittis. Aliquam et venenatis lacus. Sed facilisis ac ex non congue. Cras ligula dui, 
-      sodales ut consectetur et, bibendum sit amet lorem. Morbi sed ullamcorper diam, quis consectetur 
-      orci. Aenean sollicitudin mattis mauris.
-      Sed sed augue volutpat felis mollis auctor. Ut ac ipsum quis turpis suscipit ullamcorper et a metus. 
-      Phasellus metus erat, gravida nec magna eu, convallis mattis magna. Nullam ut lacinia nulla, nec 
-      accumsan arcu. Sed congue diam eu ipsum vulputate, quis semper metus fermentum. Vestibulum magna sem, 
-      ultricies ut diam nec, commodo mollis purus. In volutpat urna eget urna rutrum, in elementum orci 
-      fringilla. Pellentesque pretium sem erat, id egestas nibh porta a. Cras sit amet lorem dolor.
-      </p>
-      <header>
-        <Button onClick={() => exportWithObjectId()}>Show Toast</Button>
-      </header>
-        
+        </ul>
+      </div>
+      <div className="App">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consectetur facilisis nunc at 
+          imperdiet. Quisque interdum, massa vel semper euismod, libero quam lobortis lacus, a bibendum 
+          sapien diam eget nibh. Fusce elementum nec diam et consequat. Donec condimentum arcu at arcu 
+          commodo dignissim. Mauris porttitor euismod erat sed luctus. Nunc malesuada mi faucibus mauris 
+          accumsan, eu luctus velit mattis. Cras id hendrerit odio. Mauris egestas, nibh id blandit accumsan, 
+          mi enim dignissim turpis, sed lacinia sapien turpis nec velit. Donec fringilla nibh sit amet mauris
+          pellentesque sagittis. Aliquam et venenatis lacus. Sed facilisis ac ex non congue. Cras ligula dui, 
+          sodales ut consectetur et, bibendum sit amet lorem. Morbi sed ullamcorper diam, quis consectetur 
+          orci. Aenean sollicitudin mattis mauris.
+          Sed sed augue volutpat felis mollis auctor. Ut ac ipsum quis turpis suscipit ullamcorper et a metus. 
+          Phasellus metus erat, gravida nec magna eu, convallis mattis magna. Nullam ut lacinia nulla, nec 
+          accumsan arcu. Sed congue diam eu ipsum vulputate, quis semper metus fermentum. Vestibulum magna sem, 
+          ultricies ut diam nec, commodo mollis purus. In volutpat urna eget urna rutrum, in elementum orci 
+          fringilla. Pellentesque pretium sem erat, id egestas nibh porta a. Cras sit amet lorem dolor.
+        </p>
+        <header>
+          <Button onClick={() => exportWithObjectId()}>Show Toast</Button>
+        </header>
+      </div>
     </div>
   );
 }
